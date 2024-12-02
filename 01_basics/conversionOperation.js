6ba440132f1e7f237913cfcf9feb498bea3de306
rakesh765483 <rakesh765483@gmail.com>
2024-12-02 02:09:40 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,13 @@
+let score=13//number
+let scre="13"//this is string 
+console.log(typeof score);
+console.log(typeof(scre));
+let valueInNumber=Number(score)//convert string into number
+//"3"=>33
+//"33abs"=>NaN not a number
+//true=>1
+//null=>0
+//undefined=>
+//conversion to boolean 1-true
+//"" false
+//"rak" true
