8d9cfccd5b9e6d15b376bb7af47223a84f51342e
rakesh765483 <rakesh765483@gmail.com>
2024-12-02 08:20:50 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,8 @@
+console.log("2">1);//different data types still it gives correct result but sometimes it will give unpredictible result 
+//so we should make a habit to not compare data of different types 
+console.log(null>=0);
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+//strict check- it will check the value and also the data types 
+console.log("2"===2);
