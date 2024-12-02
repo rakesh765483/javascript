d8871a481a7ba72d020f0c82bdb1e9df11c7b5f3
rakesh765483 <rakesh765483@gmail.com>
2024-12-02 01:54:36 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"// treat all js code as newer version
+console.log();
+let name="rakesh"//string data types
+let age=18 //number value
+let isLoggedIn=false//bool
+//number =>2 to power 53
+//bigint
+//string may be in single or double quote
+//null-stand alone value - type object
+//undefine=> type undefined
+//symbol-used in react for 
+
+
+//objects
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId= 144553
+let accountEmail="rakesh765483@gmail.com"
+var accountPassword= "12345"
+accountCity="Jaipur"
+ let accountState
+
+// accountId=2
+accountEmail="rakeshkumarbih"
+accountPassword="5344"
+accountCity="banglore"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword,accountCity,accountState])
+/*
+prefer not to use var use let instead
+*/
