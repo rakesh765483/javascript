a4b6c1e3fcab98cd91273fed58ae0a3a25d789e7
rakesh765483 <rakesh765483@gmail.com>
2024-12-03 04:10:51 +0000

    string basics

---
+++ 01_basics/srring.js
@@ -0,0 +1,27 @@
+console.log("its me i guess")
+console.log('its me i guess')
+console.log("rakesh"+"kumar")
+console.log("rakesh"+'kumar')
+const name="rakesh"
+const repocount=56
+console.log(name+repocount+"value")
+console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+const gameName=new String('pu-bg')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('g'))
+const newString=gameName.substring(0,2);
+console.log(newString);
+const anotherString=gameName.slice(-3);
+console.log(anotherString)
+const newStringone="  rakesh  "
+console.log(newStringone)
+console.log(newStringone.trim())
+const mail="rakesh%20-76-5483";
+console.log(mail.replace('%20' ,'kk'));
+console.log(mail)
+console.log(mail.includes('rakesh'))//it tells wheather it is included in it or not
+console.log(gameName.split('-'))//split based on dash
+
