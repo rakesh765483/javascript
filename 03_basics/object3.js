d6dd8f59d6b3afbe238fe5bfecb6a7319b36fbbb
rakesh765483 <rakesh765483@gmail.com>
2024-12-04 01:10:08 +0000

    object3

---
+++ 03_basics/object3.js
@@ -0,0 +1,19 @@
+//destructuring-object and array
+const course={
+    coursename:"js in hindi",
+    price:"88",
+    instructor:"hitesh"
+}
+console.log(course.instructor)
+const{instructor}=course
+console.log(instructor)
+const{price}=course
+console.log(price)
+
+//json
+{
+    "name":"hitesh",
+    "coursename"
+
+}
+//api -will be in form of api or array
