1ed4fb39bfd0ed99691a4e27e75bea3cc43f6275
rakesh765483 <rakesh765483@gmail.com>
2024-12-08 08:32:48 +0000

    for in loop

--- loops/loop4.js
+++ loops/loop4.js
@@ -14,7 +14,21 @@
 
 //for in loop for javascript
 
-const prog=["js","rb","java"];
-for (const key in prog) {
-    console.log(key)
+// const prog=["js","rb","java"];
+// for (const key in prog) {
+//     console.log(key)
+//     console.log(prog[key])//object ka design hi isiliye hua tha taki keys ko kuch bhi asign kiya ja ske
+// }
+
+//map for in loop
+
+const map=new Map()
+map.set('IN',"india")
+map.set('USA',"united states of america")
+map.set('Fr',"france")
+for (const key in map) {
+    console.log(map[key])//map is not iterable so no value
 }
+
+//object k ander for in use krna 
+
