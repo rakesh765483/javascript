5eaee00f4c1f7ce8bc012a0d41a5532e7636f295
rakesh765483 <rakesh765483@gmail.com>
2024-12-08 06:26:36 +0000

    do while looops

---
+++ loops/loop2.js
@@ -0,0 +1,26 @@
+// //while loop
+
+// let index=0;
+// while (index<=10) {
+//     console.log(`value of index is ${index}`)
+//     index=index+2
+// }
+
+//array
+
+// let array=["flash","batman","superman"]
+// let arr=0;
+// while(arr<array.length){
+//     console.log(array[arr])
+//     arr++;
+// }
+
+//do while loops
+
+let score=11;
+
+do{
+    console.log(`score is ${score}`)
+    score++;
+
+}while(score<10)
